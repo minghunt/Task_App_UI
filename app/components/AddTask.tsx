38a425f86870c5e5f42985648842e4a7a7fab91e
minghunt <114232628+minghunt@users.noreply.github.com>
2024-04-13 23:13:28 +0700

    fix: change checkbox when add only false

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,6 +1,6 @@
 'use client'
 import dynamic from 'next/dynamic'
-import {  FormEventHandler, useState } from "react";
+import { FormEventHandler, useState } from "react";
 import { AiOutlinePlus } from 'react-icons/ai'
 import Modal from "./Modal";
 import { addTask } from '@/api';
@@ -8,38 +8,38 @@ import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react'
 
 const AddTask = () => {
-    const router = useRouter();
-    let { data: session } = useSession()
+  const router = useRouter();
+  let { data: session } = useSession()
 
-    const [modalOpen, setModalOpen] = useState<boolean>(false);
-    const [newTaskValue, setNewTaskValue] = useState<string>("");
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTaskValue, setNewTaskValue] = useState<string>("");
   const [taskCompleted, setTaskCompleted] = useState<boolean>(false);
   const handleSubmitNewTask: FormEventHandler<HTMLElement> = async (e) => {
-        e.preventDefault()
-        await addTask({
-            description: newTaskValue
-        },session?.user.token)
-        setNewTaskValue("");
-        setTaskCompleted(false)
-        setModalOpen(false);
-        router.refresh()
-    }
+    e.preventDefault()
+    await addTask({
+      description: newTaskValue
+    }, session?.user.token)
+    setNewTaskValue("");
+    setTaskCompleted(false)
+    setModalOpen(false);
+    router.refresh()
+  }
 
-    return  (
-        <div>
-          <button
-            onClick={() => setModalOpen(true)}
-            className='btn btn-primary w-full'
-          >
-            Add new task <AiOutlinePlus className='ml-2' size={18} />
-          </button>
-    
-          <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
-            <form onSubmit={handleSubmitNewTask} >
-            <h3 className='font-bold text-lg '>Add new task</h3>
-            <div className='modal-action'>
-              <div className="w-full">
-                <p className='mb-2 text-base float-left'>Name</p>
+  return (
+    <div>
+      <button
+        onClick={() => setModalOpen(true)}
+        className='btn btn-primary w-full'
+      >
+        Add new task <AiOutlinePlus className='ml-2' size={18} />
+      </button>
+
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form onSubmit={handleSubmitNewTask} >
+          <h3 className='font-bold text-lg '>Add new task</h3>
+          <div className='modal-action'>
+            <div className="w-full">
+              <p className='mb-2 text-base float-left'>Name</p>
               <input
                 value={newTaskValue}
                 onChange={(e) => setNewTaskValue(e.target.value)}
@@ -47,18 +47,18 @@ const AddTask = () => {
                 placeholder='Type here'
                 className='input input-bordered w-full'
               />
-                <div className="flex mb-2 mt-4 ">
-                    <p className='text-base mr-2 self-center'>Completed</p>
-                    <input type="checkbox" onChange={(e)=>setTaskCompleted(Boolean(e.target.value))} size={50} defaultChecked={taskCompleted} className='checkbox checkbox-secondary w-8 h-8' />
-                </div>
+              <div className="flex mb-2 mt-4 ">
+                <p className='text-base mr-2 self-center'>Completed</p>
+                <input type="checkbox" checked={false} disabled={true} className='checkbox checkbox-secondary w-8 h-8' />
               </div>
             </div>
-            <button type='submit' className='btn float-right'>
-                Submit
-              </button>
-          </form>
-          </Modal>
-        </div>
-      );
+          </div>
+          <button type='submit' className='btn float-right'>
+            Submit
+          </button>
+        </form>
+      </Modal>
+    </div>
+  );
 }
 export default dynamic(() => Promise.resolve(AddTask), { ssr: false }) 
--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -5,7 +5,7 @@ import { FormEventHandler, useEffect, useState } from "react";
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import Modal from "./Modal";
 import { useRouter } from "next/navigation";
-import { removeTask,editTask } from "@/api";
+import { removeTask, editTask } from "@/api";
 import { useSession } from 'next-auth/react'
 
 interface TaskProps {
@@ -23,16 +23,17 @@ const Task: React.FC<TaskProps> = ({ task }) => {
 
   const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    await editTask({...task,
-    description:taskToEdit,
-    completed:taskCompleted
-    },session?.user.token);
+    await editTask({
+      ...task,
+      description: taskToEdit,
+      completed: taskCompleted
+    }, session?.user.token);
     setOpenModalEdit(false);
     router.refresh();
   };
 
   const handleDeleteTask = async (_id: string) => {
-    await removeTask(_id,session?.user.token);
+    await removeTask(_id, session?.user.token);
     setOpenModalDeleted(false);
     router.refresh();
   };
@@ -40,15 +41,15 @@ const Task: React.FC<TaskProps> = ({ task }) => {
   return (
     <tr key={task._id} >
       <td className='w-4/12 '>{task.description}</td>
-      <td className='w-5 text-center'><input  type="checkbox" style={{cursor:'not-allowed'}} checked={task.completed} className="checkbox checkbox-secondary" /></td>
+      <td className='w-5 text-center'><input type="checkbox" style={{ cursor: 'not-allowed' }} checked={task.completed} className="checkbox checkbox-secondary" /></td>
       <td className='w-24 text-center'>{new Date(task.createdAt).toLocaleString('vi')}</td>
       <td className='w-24 text-center'>{new Date(task.updatedAt).toLocaleString('vi')}</td>
       <td className='flex gap-7 flex justify-center '>
         <FiEdit
-          onClick={() =>{ setOpenModalEdit(true);setTaskCompleted(task.completed);setTaskToEdit(task.description)}}
+          onClick={() => { setOpenModalEdit(true); setTaskCompleted(task.completed); setTaskToEdit(task.description) }}
           cursor='pointer'
           className='text-blue-500'
-          size={25} 
+          size={25}
         />
         <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
           <form onSubmit={handleSubmitEditTodo} >
@@ -56,22 +57,22 @@ const Task: React.FC<TaskProps> = ({ task }) => {
             <div className='modal-action'>
               <div className="w-full">
                 <p className='mb-2 text-base '>Name</p>
-              <input
-                value={taskToEdit}
-                onChange={(e) => setTaskToEdit(e.target.value)}
-                type='text'
-                placeholder='Type here'
-                className='input input-bordered w-full'
-              />
+                <input
+                  value={taskToEdit}
+                  onChange={(e) => setTaskToEdit(e.target.value)}
+                  type='text'
+                  placeholder='Type here'
+                  className='input input-bordered w-full'
+                />
                 <div className="flex mb-2 mt-4 ">
-                    <p className='text-base mr-2 self-center'>Completed</p>
-                    <input type="checkbox" onChange={(e)=>{setTaskCompleted(e.target.checked)}} size={50} checked={taskCompleted} className='checkbox checkbox-secondary w-8 h-8' />
+                  <p className='text-base mr-2 self-center'>Completed</p>
+                  <input type="checkbox" onChange={(e) => { setTaskCompleted(e.target.checked) }} size={50} checked={taskCompleted} className='checkbox checkbox-secondary w-8 h-8' />
                 </div>
               </div>
             </div>
             <button type='submit' className='btn float-right'>
-                Submit
-              </button>
+              Submit
+            </button>
           </form>
         </Modal>
         <FiTrash2
